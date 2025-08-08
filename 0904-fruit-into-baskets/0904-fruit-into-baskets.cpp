60a0d6d39fdf4ec08c3bc4b3b21bbb9b0775e4ae
vasu557 <22a91a0557@aec.edu.in>
2025-08-08 22:52:22 +0530

    Time: 52 ms (15.15%), Space: 75.1 MB (53.29%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        unordered_map<int,int>mp;
+        int maxi =0;
+        int l=0,r=0;
+        while(r < n){
+            mp[fruits[r]]++;
+            if(mp.size() > 2){
+                mp[fruits[l]]--;
+                if(mp[fruits[l]] == 0)
+                mp.erase(fruits[l]);
+                l++;
+            }
+            maxi = max(maxi,r-l+1);
+            r++;
+        }
+    return maxi;
+    }
+};
