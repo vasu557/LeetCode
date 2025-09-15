46801e0e0245dbd081a357603f6792430ce053a8
vasu557 <22a91a0557@aec.edu.in>
2025-09-15 11:22:53 +0530

    Time: 0 ms (100%), Space: 11 MB (12.71%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -13,11 +13,38 @@ class Solution {
 public:
 vector<int>ans;
     vector<int> inorderTraversal(TreeNode* root) {
-        if(root == nullptr ) return ans;
-        inorderTraversal(root->left);
-        ans.push_back(root->val);
-        inorderTraversal(root->right);
-
+        if(root == nullptr) return ans;
+        stack<TreeNode*>st;
+        TreeNode* node = root;
+        while(true){
+            if(node != nullptr){
+                st.push(node);
+                node = node->left;
+            }
+            else{
+                if(st.empty()) break;
+                node = st.top();
+                st.pop();
+                ans.push_back(node->val);
+                node = node->right;
+            }
+        }
         return ans;
     }
 };
+
+
+
+// ----------------------------------
+// class Solution {
+// public:
+// vector<int>ans;
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         if(root == nullptr ) return ans;
+//         inorderTraversal(root->left);
+//         ans.push_back(root->val);
+//         inorderTraversal(root->right);
+
+//         return ans;
+//     }
+// };
