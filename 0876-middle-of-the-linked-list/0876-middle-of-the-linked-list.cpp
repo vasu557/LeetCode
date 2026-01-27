393b92ebe85d8c70b92dad8e688c5554a5e699b4
vasu557 <22a91a0557@aec.edu.in>
2026-01-27 13:28:06 +0530

    Time: 0 ms (100%), Space: 10.1 MB (25.12%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head == nullptr) return head;
+        ListNode* temp = head;
+        int count=0;
+        while(temp){
+            count++;
+            temp = temp->next;
+        }
+        int mid = (count/2)+1;
+        temp = head;
+        count=0;
+        while(temp){
+            count++;
+            if(count == mid)
+            return temp;
+
+            temp = temp->next;
+
+        }
+        return head;
+    }
+};
