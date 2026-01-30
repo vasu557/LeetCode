acbbc08e373ee7f850c09be0f312f2fa7b4f9bec
vasu557 <22a91a0557@aec.edu.in>
2026-01-30 18:05:32 +0530

    Time: 2 ms (55.15%), Space: 15.3 MB (33.49%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans ="";
+        stringstream ss(s);
+        string word;
+        while(ss >> word){
+            reverse(word.begin(),word.end());
+            ans += word;
+            ans+= " ";
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
