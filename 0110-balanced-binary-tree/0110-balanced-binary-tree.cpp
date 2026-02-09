fbc6b29f526070f4c3cd0d44b19e783fe249ad53
vasu557 <22a91a0557@aec.edu.in>
2026-02-09 21:22:43 +0530

    Time: 0 ms (100%), Space: 23.2 MB (20.59%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -10,19 +10,18 @@
  * };
  */
 class Solution {
-public:
     int check(TreeNode* root){
         if(root == nullptr) return 0;
         int lh = check(root->left);
         int rh = check(root->right);
-        if(lh ==-1 || rh == -1) return -1;
+        if(lh == -1 || rh == -1) return -1;
         if(abs(lh-rh) > 1) return -1;
-        return 1+max(lh,rh);
+        return 1 + max(lh,rh);
     }
-
-
+public:
     bool isBalanced(TreeNode* root) {
-        if(root == nullptr) return true;
-       return  check(root) != -1;
+        if(root ==nullptr) return true;
+
+        return check(root) != -1;
     }
 };
