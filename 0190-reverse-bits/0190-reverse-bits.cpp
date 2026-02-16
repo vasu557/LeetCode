d32ea8b4e39d0e979736d2765f82684d42d21804
vasu557 <22a91a0557@aec.edu.in>
2026-02-16 12:44:08 +0530

    Time: 0 ms (100%), Space: 8.4 MB (9.32%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -2,32 +2,23 @@ class Solution {
 public:
     int reverseBits(int n) {
         string s ="";
-        while(n>0){
-            if(n % 2 == 0){
+        for(int i=0;i<32;i++){
+            if(n% 2 == 0){
                 s+='0';
             }
             else{
                 s+='1';
             }
-        n=n/2;
+            n = n>>1;
         }
-        int temp=0;
-        if(s.length()<32){
-        temp = 32 - s.length();
-        }
-        for(int i=0;i<temp;i++){
-            s+='0';
-        }
-        int ans=0;
-        unsigned int po = 1;
-
+        unsigned int temp=1;
+        unsigned int ans=0;
         for(int i=s.length()-1;i>=0;i--){
-            if(s[i]=='1'){
-                ans+=po;
-            }
-            po=po*2;
-        }
+            if(s[i] == '1')
+            ans+=temp;
 
-    return ans;
+            temp*=2;
+        }
+        return ans;
     }
 };
