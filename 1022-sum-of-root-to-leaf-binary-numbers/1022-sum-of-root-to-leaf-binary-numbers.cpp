b91ed0aeb7879b91c047fa8e031c142270495f89
vasu557 <22a91a0557@aec.edu.in>
2026-02-24 09:56:09 +0530

    Time: 0 ms (100%), Space: 18.7 MB (25.26%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -11,31 +11,38 @@
  */
 class Solution {
 public:
-void getPath(TreeNode* root,vector<vector<int>>&vec,vector<int>&temp){
-    if(root == nullptr) return;
-    temp.push_back(root->val);
-    if(root != nullptr && !root->left && !root->right)
-    vec.push_back(temp);
-    getPath(root->left,vec,temp);
-    getPath(root->right,vec,temp);
-    temp.pop_back();
-}
+    void preorder(TreeNode* root,vector<vector<int>>&vec,vector<int>&temp){
+        if(root == nullptr){
+            return ;
+        }
+        
+        temp.push_back(root->val);
+        if(root->left == nullptr && root->right == nullptr){
+            vec.push_back(temp);
+        }
+        preorder(root->left,vec,temp);
+        preorder(root->right,vec,temp);
+
+        temp.pop_back();
+
+    }
     int sumRootToLeaf(TreeNode* root) {
-        if(!root) return 0;
+        if(root == nullptr) return 0;
+        int sum =0;
         vector<vector<int>>vec;
         vector<int>temp;
-        getPath(root,vec,temp);
-        int sum =0;
-        for(auto &ans : vec){
-            int n=ans.size();
+        preorder(root,vec,temp);
+        for(int i=0;i<vec.size();i++){
+            int n=vec[i].size();
             int po=1;
-            int cur =0;
-            for(int i=n-1;i>=0;i--){
-                if(ans[i]==1)
-                cur = cur+po;
-                po = po*2;
-            }   
-            sum +=cur;
+            int cur=0;
+            for(int j=n-1;j>=0;j--){
+                if(vec[i][j] == 1)
+                cur+=po;
+
+                po*=2;
+            }
+            sum+=cur;
         }
         return sum;
     }
