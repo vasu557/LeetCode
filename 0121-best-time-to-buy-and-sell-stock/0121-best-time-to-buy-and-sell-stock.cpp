78d6fbcb40135ad3cf8e09857c03efcd605f872a
vasu557 <22a91a0557@aec.edu.in>
2026-03-13 19:01:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int mini =INT_MAX;
-        int profit =0;
-        for(int i=0;i<prices.size();i++){
+        int n = prices.size();
+        int profit = 0;
+        int mini=INT_MAX;
+        for(int i=0;i<n;i++){
             if(prices[i] < mini)
             mini = prices[i];
             else
             profit = max(profit,prices[i]-mini);
         }
-        return profit;
+    return profit;
     }
 };
