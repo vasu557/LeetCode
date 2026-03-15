a2e6a3734ed03be12c5e4d4c007e81f88fe1b413
vasu557 <22a91a0557@aec.edu.in>
2026-03-15 23:41:03 +0530

    Time: 7 ms (19.52%), Space: 9.7 MB (73.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int arr[26];
-        fill(arr,arr+26,0);
-        for(int i=0;i<s.length();i++){
-            arr[s[i] - 'a']++;
-        }
-        for(int i=0;i<t.length();i++){
-            arr[t[i] - 'a']--;
-        }
-        for(int i=0;i<26;i++){
-            if(arr[i] != 0) return false;
-        }
-    return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s == t;
     }
 };
