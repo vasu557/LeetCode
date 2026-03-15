7efd9ebe4671eb3261e5ac737e929086109d15da
vasu557 <22a91a0557@aec.edu.in>
2026-03-15 23:38:46 +0530

    Time: 23 ms (30.79%), Space: 26.2 MB (37.9%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,60 +1,16 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n =strs.size();
-        vector<vector<string>>res;
+        vector<vector<string>>ans;
         unordered_map<string,vector<string>>mp;
-        for(int i=0;i<n;i++){
-            string s = strs[i];
-            sort(s.begin(),s.end());
-            mp[s].push_back(strs[i]);
+        for(int i=0;i<strs.size();i++){
+            string temp = strs[i];
+            sort(temp.begin(),temp.end());
+            mp[temp].push_back(strs[i]);
         }
-        for(auto it:mp){
-            res.push_back(it.second);
+        for(auto it : mp){
+            ans.push_back(it.second);
         }
-    return res;
+    return ans;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-// public:
-//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-//         int n=strs.size();
-//         vector<bool>visited(n,false);
-//         vector<vector<string>>pair;
-//         for(int i=0;i<n;i++){
-//             if(visited[i]) continue;
-//             vector<string>st;
-//             string s = strs[i];
-//             sort(s.begin(),s.end());
-//             for(int j=i;j<n;j++){
-//                 if(visited[j]) continue;
-//                 string s2 = strs[j];
-//                 sort(s2.begin(),s2.end());
-//                 if(s == s2){
-//                     st.push_back(strs[j]);
-//                     visited[j] = true;
-//                 }
-//             }
-//             pair.push_back(st);
-//         }
-//     return pair;
-//     }
-// };
