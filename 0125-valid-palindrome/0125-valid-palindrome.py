266ae9f44dc43387f2af446a3ead092cb9b52eec
vasu557 <vasurongala11@gmail.com>
2026-04-06 13:24:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,13 +1,4 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s=s.replace(" ","")
-        temp =""
-        for i in range(len(s)):
-            if(s[i].isalnum()):
-               temp+=s[i].lower()
-
-        rev = temp[::-1]
-        if temp == rev:
-            return True;
-        else:
-           return False;
+        temp = ''.join(ch.lower() for ch in s if (ch.isalnum()))
+        return temp == temp[::-1];
